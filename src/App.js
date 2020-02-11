807e92ad8bd7e393e29ea16846b7667b63f76abd
xiaozhang222 <https://github.com/h50919-third-team/12.21.git>
2020-02-11 21:31:29 +0800

    更改

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class app extends Component {
+    render() {
+        return (
+            <h1>
+                App
+            </h1>
+        )
+    }
+}
