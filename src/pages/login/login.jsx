1738f6f6ed380cc4aada77400ce029049dc297ff
xiaozhang222 <https://github.com/h50919-third-team/12.21.git>
2020-02-12 01:11:36 +0800

    gengxi

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
+import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import logo from './img/logo.png'
+import './css/login.css'
 
 export default class Login extends Component {
     render() {
         return (
-            <h1>
-                Login
-            </h1>
+            <div id="login">
+                <div className="header">
+                    <h1>
+                        <img src={logo} alt=""/>
+                    </h1>
+                    <h1>商品管理系统</h1>
+                </div>
+                <div className="content">
+                    <h1>用户登陆</h1>
+                    <div>
+                        antd表单
+                    </div>
+                </div>
+            </div>
         )
     }
 }
